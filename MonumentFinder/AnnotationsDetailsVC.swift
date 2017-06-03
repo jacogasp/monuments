91a6c1bb85aec848dec16564a250befbd22ad1bb
Jacopo Gasparetto <Chioma@p200300728d397e001c51d8966d311ac3.dip0.t-ipconnect.de>
2017-06-03 19:34:26 +0200

    boh

--- MonumentFinder/AnnotationsDetailsVC.swift
+++ MonumentFinder/AnnotationsDetailsVC.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import SwiftyJSON
+import Alamofire
+
 class AnnotationDetailsVC: UIViewController {
     
     var titolo: String?
@@ -28,9 +31,13 @@ class AnnotationDetailsVC: UIViewController {
         
         // Do any additional setup after loading the view.
         self.view.backgroundColor = UIColor.clear
+        
         titleLabel.text = titolo ?? "Nessun titolo"
         categoryLabel.text = categoria ?? "Nessuna categoria"
-
+        
+        if titolo != nil {
+            getWikiSummary(titolo: titolo!)
+        }
         
     }
 
@@ -40,6 +47,71 @@ class AnnotationDetailsVC: UIViewController {
     }
     
     
+    func getWikiSummary(titolo: String) {
+        print("Start query...\n")
+        let parameters = [
+            "action": "query",
+            "format": "json",
+            "prop": "extracts|pageimages",
+            "list": "",
+            "titles": titolo,
+            "exintro": 1,
+            "explaintext": 1,
+            "pithumbsize": "300"
+
+        ] as [String : Any]
+        
+        // https://en.wikipedia.org/w/api.php?action=query&titles=Alban%20Stolz&prop=pageimages&pithumbsize=300
+        
+        let url = "https://en.wikipedia.org/w/api.php"
+        let urlImg = ""
+        
+        Alamofire.request(url, parameters: parameters).responseJSON { response in
+            print("\(String(describing: response.request))")
+            if let value = response.result.value {
+                print("\(value)")
+                
+                let json = JSON(value)
+                if let pages = json["query"]["pages"].dictionary {
+                    if let page = pages.first {
+                        let details = page.value
+                        
+                        let extract = details["extract"].stringValue
+                        if extract != "" {
+                            self.textField.text = extract
+                        }
+                        
+                        let thumbnail = details["thumbnail"]["source"].stringValue
+                        if thumbnail != "" {
+                            
+                        }
+                        
+                    } else {
+                        self.textField.text = "Nessuna informazione."
+                    }
+                } else {
+                  self.textField.text = "Nessuna informazione."
+                }
+            } else {
+                self.textField.text = "Nessuna informazione."
+            }
+        }
+
+    }
+    
+   /* func getWikiPicture(url: String) {
+        Alamofire.request("https://httpbin.org/image/png").responseImage { response in
+            debugPrint(response)
+            
+            print(response.request)
+            print(response.response)
+            debugPrint(response.result)
+            
+            if let image = response.result.value {
+                print("image downloaded: \(image)")
+            }
+        }
+    }*/
 
     
 }
