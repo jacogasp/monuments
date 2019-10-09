3ca393d0610044324c9210fdf747d0f0b741445d
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2019-10-09 12:57:08 +0200

    Urgent fix Core Data files

--- Monuments/Core Data/MonumentExtension.swift
+++ Monuments/Core Data/MonumentExtension.swift
@@ -1,16 +1,9 @@
 //
-//  Monument+CoreDataClass.swift
+//  MonumentExtension.swift
 //  Monuments
 //
 //  Created by Jacopo Gasparetto on 09/10/2019.
 //  Copyright © 2019 Jacopo Gasparetto. All rights reserved.
 //
-//
 
 import Foundation
-import CoreData
-
-
-public class Monument: NSManagedObject {
-
-}
