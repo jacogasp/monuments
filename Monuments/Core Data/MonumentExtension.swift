81f327858f2942c356c8da63a88fc87a7aa290c9
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2019-10-15 13:17:28 +0200

    New location computed property

--- Monuments/Core Data/MonumentExtension.swift
+++ Monuments/Core Data/MonumentExtension.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Jacopo Gasparetto. All rights reserved.
 //
 
-import Foundation
+import CoreLocation
+
+extension Monument {
+    @objc dynamic var location: CLLocation{
+        return CLLocation(latitude: self.latitude, longitude: self.longitude)
+    }
+    
+    override public class func keyPathsForValuesAffectingValue(forKey key: String) -> Set<String>{
+        let keyPaths = super.keyPathsForValuesAffectingValue(forKey: key)
+        switch key {
+        case "location":
+            return keyPaths.union(Set(["latitude", "longitude"]))
+        default:
+            return keyPaths
+        }
+    }
+}
