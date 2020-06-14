ce72a99ccc8b4c9b4e8ce90b203ec70b9b91daad
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2020-06-14 12:32:11 +0200

    Update graphics

---
+++ Monuments/Commons/Protocols.swift
@@ -0,0 +1,9 @@
+//
+//  Protocols.swift
+//  Monuments
+//
+//  Created by Jacopo Gasparetto on 26/04/2020.
+//  Copyright © 2020 Jacopo Gasparetto. All rights reserved.
+//
+
+import Foundation
---
+++ Monuments/Views/SideMenuViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SideMenuViewCell.swift
+//  Monuments
+//
+//  Created by Jacopo Gasparetto on 02/05/2020.
+//  Copyright © 2020 Jacopo Gasparetto. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
