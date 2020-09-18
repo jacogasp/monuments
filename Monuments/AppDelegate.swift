b439084aa129990805a62f8b1e762acca6ff9b01
Johnny Parafango <jacopo.gasparetto@gmail.com>
2020-09-18 21:56:21 +0200

    Conversion to AppDelegate + SceneDelegate

--- Monuments/AppDelegate.swift
+++ Monuments/AppDelegate.swift
@@ -1,31 +1,23 @@
 //
 //  AppDelegate.swift
-//  MonumentFinder
+//  Monuments
 //
-//  Created by Jacopo Gasparetto on 08/01/2017.
-//  Copyright © 2017 Jacopo Gasparetto. All rights reserved.
+//  Created by Jacopo Gasparetto on 07/09/2020.
+//  Copyright © 2020 Jacopo Gasparetto. All rights reserved.
 //
 
 import UIKit
-import CoreData
 import SwiftyBeaver
 import CoreLocation.CLLocationManager
 import AVFoundation.AVCaptureDevice
-import SwiftUI
 
 let logger = SwiftyBeaver.self
-let preloadDataKey = "didPreloadData"
-let selectedCategoriesKey = "selectedCategories"
-
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-    var authorizationsNeeded: [AuthorizationRequestType]?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        // Override point for customization after application launch.
         let console = ConsoleDestination()
         console.levelColor.debug = "🐞 "
         console.levelColor.error = "❌ "
@@ -33,124 +25,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         console.levelColor.verbose = "📣 "
         console.levelColor.warning = "⚠️ "
         logger.addDestination(console)
-        
+
         logger.info("Running application...\n\n")
-        
-        
-        // Environment
-        let env = Environment()
-        logger.info("Found \(env.activeCategories.filter{$0.isSelected}.count) active categories")
-                
-        // Wait for Launch Screen
-        Thread.sleep(forTimeInterval: 1.0)
-        
-        // Decide initial controller
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        var viewController: UIViewController
-        
-        // Set default tint color
-        self.window?.tintColor = EnvironmentConfiguration().defaultColor
-        
-        // On boarding
-        if let authorizationsNeeded = authorizationRequestsNeeded() {
-            let onboardingViewController = storyBoard.instantiateViewController(identifier: "OnboardingViewController") as! OnboardingViewController
-            onboardingViewController.authorizationsNeeded = authorizationsNeeded
-            viewController = onboardingViewController
-            viewController.endAppearanceTransition()
-            
-        } else {
-            viewController = UIViewController()
-        }
-        
-        self.window?.rootViewController = UIHostingController(rootView: MonumentsView().environmentObject(env))
-        
-        self.window?.makeKeyAndVisible()
-        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types
-		// of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits
-		// the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks.
-		// Games should use this method to pause the game.
+    // MARK: UISceneSession Lifecycle
 
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers,
-		// and store enough application state information to restore your application to its current state in case
-		// it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate:
-		// when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the
-		// changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        let defaults = UserDefaults.standard
-        defaults.removeObject(forKey: "mapViewRegion")
-        defaults.synchronize()
-    }
-
-    // MARK: - Preload Data
-     
-    func loadPlistFile<T>(forResource resource: String, forType type: T.Type) -> T where T: Decodable {
-         guard let plistUrl = Bundle.main.url(forResource: resource, withExtension: "plist") else {
-             fatalError("Cannot locate file \(resource).plist")
-         }
-         do {
-             let plistData = try Data(contentsOf: plistUrl)
-             let decoder = PropertyListDecoder()
-             let decodedData = try decoder.decode(type.self, from: plistData)
-             return decodedData
-         } catch {
-             fatalError("Cannot decode data, error: \(error)")
-         }
-     }
-    
-    func encodeWikipediaToJSON(wikiData: [String:String]?) -> String? {
-        if let wikiData = wikiData {
-            do {
-                let json = try JSONSerialization.data(withJSONObject: wikiData, options: [])
-                return String(data: json, encoding: .utf8)
-            } catch {
-                print(error)
-            }
-        }
-        return nil
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
+
 // MARK: - Authorizations
 
 enum AuthorizationRequestType {
     case location, camera
 }
- 
+
 extension AppDelegate {
-    
+
     private func authorizationRequestsNeeded() -> [AuthorizationRequestType]? {
         var requests: [AuthorizationRequestType]?
-        
+
         let locationStatus = CLLocationManager.authorizationStatus()
         let cameraStatus = AVCaptureDevice.authorizationStatus(for: .video)
-        
+
         let locationNeeded = locationStatus == .notDetermined || locationStatus == .restricted || locationStatus == .denied
         let cameraNeeded = cameraStatus == .notDetermined || cameraStatus == .restricted || cameraStatus == .denied
-        
+
         if locationNeeded { requests?.append(.location) ?? (requests = [.location]) }
         if cameraNeeded { requests?.append(.camera) ?? (requests = [.camera])}
         return requests
     }
 }
-
