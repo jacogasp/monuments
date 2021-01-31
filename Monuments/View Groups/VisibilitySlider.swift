f462a62d6596db25d85077e9e7d1c9b8968f6890
JohnnyParafango <jacopo.gasparetto@ammagamma.com>
2021-01-31 11:44:04 +0100

    Fix: rounder corners visibilitySlider

--- Monuments/View Groups/VisibilitySlider.swift
+++ Monuments/View Groups/VisibilitySlider.swift
@@ -22,7 +22,7 @@ struct VisibilitySlider: View {
     @EnvironmentObject private var env: Environment
     
     var rangeHeight: CGFloat {
-        return buttonHeight - innerOffset / 2
+        buttonHeight - innerOffset / 2
     }
     
     var tap: some Gesture {
@@ -32,13 +32,13 @@ struct VisibilitySlider: View {
                     self.isOpen = true
                 }
                 DispatchQueue.main.asyncAfter(timeInterval: 1, execute: {
-                    if self.offset == .zero {
+                    if offset == .zero {
                         withAnimation {
                             self.isOpen = false
                         }
                     }
                 })
-        }
+            }
     }
     
     var drag: some Gesture {
@@ -47,56 +47,53 @@ struct VisibilitySlider: View {
                 let translationY = gesture.translation.height
                 self.offset = CGSize(
                     width: 0,
-                    height: min(max(-self.rangeHeight, translationY), self.rangeHeight)
+                    height: min(max(-rangeHeight, translationY), rangeHeight)
                 )
-                if !self.isZooming {
+                if !isZooming {
                     self.isZooming = true
                 }
-        }
-        .onEnded { _ in
-            self.isZooming = false
-            withAnimation {
-                self.offset = .zero
             }
-            DispatchQueue.main.asyncAfter(timeInterval: 1, execute: {
+            .onEnded { _ in
+                self.isZooming = false
                 withAnimation {
-                    self.isOpen = false
+                    self.offset = .zero
                 }
-            })
-            
-        }
+                DispatchQueue.main.asyncAfter(timeInterval: 1, execute: {
+                    withAnimation {
+                        self.isOpen = false
+                    }
+                })
+            }
     }
     
     var body: some View {
         let combined = tap.sequenced(before: drag)
         
-        if self.isZooming {
+        if isZooming {
             DispatchQueue.main.async {
-                var increment = pow(abs(Double(self.offset.height) / 100), self.speedFactor)
-                increment = self.offset.height > 0 ? increment * -1 : increment
-                self.env.maxDistance = max(min(self.env.maxDistance + increment, 5000), 0)
+                var increment = pow(abs(Double(offset.height) / 100), speedFactor)
+                increment = offset.height > 0 ? increment * -1 : increment
+                env.maxDistance = max(min(env.maxDistance + increment, 5000), 0)
             }
         }
         
         return HStack {
-            Group {
-                Text("\(Int(self.env.maxDistance)) m")
-                    .font(Font.subtitle)
-                    .padding(.horizontal, 8)
-                    .padding(.vertical, 2)
-            }
-            .background(Color.white)
-            .opacity(self.isOpen ? 1 : 0)
-            .cornerRadius(.infinity)
-            .offset(self.offset)
-            .padding(.trailing)
+            Text("\(Int(env.maxDistance)) m")
+                .font(Font.subtitle)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 2)
+                .background(Color.white)
+                .clipShape(Capsule(style: .circular))
+                .opacity(isOpen ? 1 : 0)
+                .offset(offset)
+                .padding(.trailing)
             
             ZStack {
                 RoundedRectangle(cornerRadius: width / 2)
                     .foregroundColor(Color.white.opacity(0.8))
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(.gray)
-                    .opacity(self.isOpen ? 0 : 1)
+                    .opacity(isOpen ? 0 : 1)
                 ZStack {
                     RoundedRectangle(cornerRadius: (width - innerOffset) / 2)
                         .foregroundColor(.white)
@@ -106,21 +103,20 @@ struct VisibilitySlider: View {
                         Spacer()
                         Image(systemName: "chevron.down")
                     }
-                    .padding(.top)
-                    .padding(.bottom)
+                        .padding(.top)
+                        .padding(.bottom)
                 }
-                .frame(height: self.isOpen ? buttonHeight : 0)
-                .opacity(self.isOpen ? 1 : 0)
-                .offset(self.offset)
-                .shadow(radius: 9, y: 4)
-                .padding(.leading, 4)
-                .padding(.trailing, 4)
-                
+                    .frame(height: isOpen ? buttonHeight : 0)
+                    .opacity(isOpen ? 1 : 0)
+                    .offset(offset)
+                    .shadow(radius: 9, y: 4)
+                    .padding(.leading, 4)
+                    .padding(.trailing, 4)
             }
-            .gesture(combined)
-            .frame(width: width, height: self.isOpen ? height : width)
+                .gesture(combined)
+                .frame(width: width, height: isOpen ? height : width)
         }
-        .padding()
+            .padding()
     }
 }
 
@@ -131,7 +127,7 @@ struct VisibilitySliderTest: View {
             HStack {
                 Spacer()
                 VisibilitySlider()
-                .environmentObject(Environment())
+                    .environmentObject(Environment())
             }
         }
     }
